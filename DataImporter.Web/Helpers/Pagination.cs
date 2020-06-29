6d01f51c0cb765460831042cbe200a6ed83b7742
butsayura <butsayura@gmail.com>
2020-06-29 20:49:55 +0300

    Added Transactions Search

---
+++ DataImporter.Web/Helpers/Pagination.cs
@@ -0,0 +1,29 @@
+﻿using JW;
+using System;
+
+namespace DataImporter.Web.Helpers
+{
+    public class Pagination
+    {
+        public Pagination(string baseUrl, string pageTag, int totalItems, int currentPage, int pageSize = 10, int maxPages = 10)
+        {
+            this.Pager = new Pager(totalItems, currentPage, pageSize);
+            this.BaseUrl = baseUrl;
+            this.PageTag = pageTag;
+        }
+
+        public Pagination(Func<int, string> onClick, int totalItems, int currentPage, int pageSize = 10, int maxPages = 10)
+        {
+            this.Pager = new Pager(totalItems, currentPage, pageSize);
+            this.OnClick = onClick;
+        }
+
+        public Pager Pager { get; set; }
+
+        public string BaseUrl { get; set; }
+
+        public string PageTag { get; set; }
+
+        public Func<int, string> OnClick { get; set; }
+    }
+}
--- DataImporter.Web/Pages/Transactions/Search.cshtml.cs
+++ DataImporter.Web/Pages/Transactions/Search.cshtml.cs
@@ -1,11 +1,79 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using DataImporter.Entities;
+using DataImporter.Models;
+using DataImporter.Repositories;
+using DataImporter.Web.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DataImporter.Web.Pages.Transactions
 {
     public class SearchModel : PageModel
     {
-        public void OnGet()
+        private readonly ITransactionRepository transactionRepository;
+
+        public SearchModel(ITransactionRepository transactionRepository)
         {
+            this.transactionRepository = transactionRepository;
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public CurrencyCode? Currency { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public TransactionStatus? Status { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public DateTime? FromDate { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public DateTime? ToDate { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int PageNumber { get; set; } = 1;
+
+        public TransactionModel[] Transactions { get; private set; }
+
+        public Pagination Pagination { get; private set; }
+
+        public async Task OnGet()
+        {
+            this.PageNumber = Math.Max(this.PageNumber, 1);
+
+            const int takeCount = 10;
+
+            var result = await this.transactionRepository.SearchTransaction(
+                new TransactionSearchOptions
+                {
+                    CurrencyCode = this.Currency,
+                    Status = this.Status,
+                    FromDate = this.FromDate,
+                    ToDate = this.ToDate,
+                    Skip = (this.PageNumber - 1) * takeCount,
+                    Take = takeCount,
+                });
+
+            this.Transactions = result.Items.Select(x =>
+                new TransactionModel
+                {
+                    Id = x.Id,
+                    Payment = $"{x.Amount:0.00} {x.CurrencyCode}",
+                    Status = x.Status.ToString().Substring(0, 1),
+                })
+                .ToArray();
+
+            this.Pagination = new Pagination((page) => $"filter({page})", result.Count, this.PageNumber, takeCount);
+        }
+
+        public class TransactionModel
+        {
+            public string Id { get; set; }
+
+            public string Payment { get; set; }
+
+            public string Status { get; set; }
         }
     }
 }
